a32e689c64b15e8df804575652326cfffecd01dc
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-03-03 16:23:20 -0400

    Add background image to text and additional image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'next client'
 
 import Head from "next/head";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -9,7 +10,21 @@ const Home = () => {
         <title>Página Principal</title>
       </Head>
       <div>
-        <h1 className="text-4xl text-red-800">Hello Adriana</h1>
+        <h1 
+          className="text-4xl text-white bg-cover bg-center p-5 mb-5"
+          style={{ backgroundImage: "url('/images/pattern.jpg')" }}>
+            Hello Adriana
+        </h1>
+        
+        <div className="flex justify-center items-center">
+          <Image 
+            src="/images/city.jpg" 
+            width={200} 
+            height={200}
+            alt="Cidade a Noite"
+            className="rounded-lg"
+          />
+        </div>
       </div>
     </>
     
