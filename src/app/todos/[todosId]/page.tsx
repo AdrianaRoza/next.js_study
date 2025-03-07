94ae77096d4f42fc1275eba62971b19c6301751d
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-03-07 12:06:46 -0400

    dynamic routes with data

--- src/app/todos/[todosId]/page.tsx
+++ src/app/todos/[todosId]/page.tsx
@@ -1,16 +1,35 @@
-interface TodoParams{
-  params:{
-    todosId: number
-  }
+interface Todo {
+  id: number
+  title: string
+  completed: boolean
 }
 
+async function getTodo(todosId: string): Promise<Todo | null> {
+  const response = await fetch
+    (`https://jsonplaceholder.typicode.com/todos/${todosId}`)
 
-const TodosId = ({ params }: TodoParams) =>{
-  return(
+  if (!response.ok) {
+    return null
+  }
+
+  return response.json()
+}
+
+export default async function 
+  TodosId({ params }: { params: { todosId: string } }) {
+  
+    const todo: Todo | null = await getTodo(params.todosId)
+
+  if (!todo) {
+    return <p> Tarefa não encontrada! Escolha uma tarefa válida.</p>
+  }
+
+  return (
     <div>
-      <p>Informacoes {params.todosId} </p>
+      <p>Informações do TODO:</p>
+      <p><strong>ID:</strong> {todo.id}</p>
+      <p><strong>Título:</strong> {todo.title}</p>
+      <p><strong>Concluído:</strong> {todo.completed ? "Sim" : "Não"}</p>
     </div>
   )
 }
-
-export default TodosId
